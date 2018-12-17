939a3b8694a624cbfc2376f33c27af4dd8299631
Tomer Landesman <tomer.landesman@Tomers-Mac-mini.local>
2018-12-17 10:35:41 +0200

    improved graphics

---
+++ DataStructHw4/Views/WeatherDisplayView.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherDisplayView.swift
+//  DataStructHw4
+//
+//  Created by Tomer Landesman on 12/12/18.
+//  Copyright © 2018 Tomer Landesman. All rights reserved.
+//
+
+import Foundation
